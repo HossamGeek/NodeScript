6a2657d7daf2053b5a0877a1e10ac53d2d38b0ed
unknown <hossam.mahmoud.azdan@gmail.com>
2019-09-15 17:33:55 +0200

    add graphql

--- BackEnd/src/app.config.ts
+++ BackEnd/src/app.config.ts
@@ -1,9 +1,8 @@
 import {Application} from 'express';
 import Connected from './db.config';
 import bodyParser from 'body-parser';
+import graphqlHttp from 'express-graphql';
 
-const midParse = bodyParser.urlencoded({ extended: true });
- 
 const AppConfig = (app:Application) =>{
     Connected.on('error', (err)=>{
         console.log('DataBase connection error!.\n'
@@ -16,10 +15,10 @@ const AppConfig = (app:Application) =>{
     Connected.on('open', (err)=>{
         console.log("DataBase's connected.\n" +
                      "You can start working successfully ...");
-        app.use('/user',midParse,(req,res,next)=>{
-            // let new_user = new User({	"email":"hossam_mahmoud@gmail.com"});
-            //  new_user.save().then(result=> res.json(result));
-        })             
+        // app.use('/api',graphqlHttp({
+        //     schema:{},
+        //     graphiql:true
+        // }))             
     });    
 }
 
